db24eef365f572076504aad3ea53c77b0b5bf6b7
xiangxian555-dotcom <xiangxian555@gmail.com>
2026-03-11 18:03:20 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ios: resolve(__dirname, 'ios.html'),
+      }
+    }
+  }
+})
